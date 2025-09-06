4a6d8256d8260df335995a9aaf2802596c2fe0d9
mghasemi5 <mohammadghasemi5@gmail.com>
2025-09-06 16:51:13 +0330

    v1.1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -125,8 +125,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static-backend/"
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+# where collectstatic will put files
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# additional static dirs for dev
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
